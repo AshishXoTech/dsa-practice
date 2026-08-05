cab01cf3af83c76e5f9efceda3d10b37dcd39392
ASHISH KUMAR JHA <ashish863863@gmail.com>
2026-08-05 22:59:36 +0530

    Time: 15 ms (59.86%), Space: 51 MB (97.62%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -5,7 +5,7 @@ class Solution {
         int i = 0;
         int j = 0;
         while (i < g.length && j < s.length) {
-            if (s[j] > g[i]) {
+            if (s[j] >= g[i]) {
                 i++;
             }
             j++;
